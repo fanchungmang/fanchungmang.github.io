b64aaffbde12dcc894a861be535fd5839d55dc1a
Marcus Fan <marcuscmfan2@gmail.com>
2024-11-29 12:33:34 -0500

    adding files

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <img src="./assets/Marcus Fan.png" class="logo" alt="Vue logo" />
+  </div>
+  <div id="Landing">
+    <table>
+      <th>
+        <pre>Hello, my name is Marcus. I am currently a First Year Engineering student at the University of Western Ontario</pre>
+      </th>
+      <th>
+      </th>
+    </table>
+  </div>
+  <div id="Experience">
+    <pre>Hello</pre>
+  </div>
+
+  <div id="Contact">
+    <pre>Hello</pre>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
